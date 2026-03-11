9acb03b40eeff1ac51eb827c5a459bfddebb2f67
zoo-brong <answngud1128@gmail.com>
2026-03-11 18:21:07 +0900

    fix: [DM-27] 참조문서 응답 필드 수정
    [DM-27]

--- src/main/java/kr/java/documind/domain/chatbot/model/dto/response/ReferenceResponse.java
+++ src/main/java/kr/java/documind/domain/chatbot/model/dto/response/ReferenceResponse.java
@@ -1,7 +1,6 @@
 package kr.java.documind.domain.chatbot.model.dto.response;
 
 public record ReferenceResponse(
-        int rank,
         Long documentId,
         String documentName,
         String extension,
