864b5838d7cb18552e8ca4c786682b447afbe4b5
zoo-brong <answngud1128@gmail.com>
2026-03-16 14:23:52 +0900

    fix: [DM-27] @Transactional(readOnly = true) 추가
    [DM-27]

--- src/main/java/kr/java/documind/domain/chatbot/service/ChatbotMetaService.java
+++ src/main/java/kr/java/documind/domain/chatbot/service/ChatbotMetaService.java
@@ -7,9 +7,11 @@ import kr.java.documind.domain.chatbot.model.dto.response.ChatModelInfoResponse;
 import kr.java.documind.domain.chatbot.properties.SupportedChatModels;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ChatbotMetaService {
 
     private final SupportedChatModels supportedChatModels;
