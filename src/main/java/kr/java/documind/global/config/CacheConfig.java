5a8fffeffbd520a34d864a8821ec5353716190df
ParkGoeun00 <goeuni33@gmail.com>
2026-03-16 14:53:22 +0900

    feat: [DM-50] 캐시컨피그 기본 직렬화로 수정 DM-50

--- src/main/java/kr/java/documind/global/config/CacheConfig.java
+++ src/main/java/kr/java/documind/global/config/CacheConfig.java
@@ -1,5 +1,10 @@
 package kr.java.documind.global.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.time.Duration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +22,20 @@ public class CacheConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.activateDefaultTyping(
+                BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build(),
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY);
+
+        objectMapper.setTypeFactory(
+                TypeFactory.defaultInstance()
+                        .withClassLoader(Thread.currentThread().getContextClassLoader()));
+
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(objectMapper);
+
         RedisCacheConfiguration projectSelectorConfig =
                 RedisCacheConfiguration.defaultCacheConfig()
                         .entryTtl(Duration.ofSeconds(60))
@@ -25,7 +44,7 @@ public class CacheConfig {
                                         new StringRedisSerializer()))
                         .serializeValuesWith(
                                 RedisSerializationContext.SerializationPair.fromSerializer(
-                                        new GenericJackson2JsonRedisSerializer()));
+                                        serializer));
 
         return RedisCacheManager.builder(connectionFactory)
                 .withCacheConfiguration("projectSelector", projectSelectorConfig)
