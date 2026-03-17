aebe05c456cf189538f464595b5e1335c19b3988
ParkGoeun00 <goeuni33@gmail.com>
2026-03-17 16:25:07 +0900

    feat: [DM-52] quert dsl config 추가
    DM-52

---
+++ src/main/java/kr/java/documind/global/config/QueryDslConfig.java
@@ -0,0 +1,14 @@
+package kr.java.documind.global.config;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
+}
